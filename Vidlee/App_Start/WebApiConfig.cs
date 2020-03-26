12c3e0fc86b0cc19a55aca30dece091ef50cfa3b
seanCalvey <calsean86@outlook.ie>
2020-03-26 20:49:43 +0000

    Finish Customer Api

---
+++ Vidlee/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Vidlee
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var Setting = config.Formatters.JsonFormatter.SerializerSettings;
+            Setting.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            Setting.Formatting = Formatting.Indented;
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Vidlee/Dto/CustomerDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Vidlee.Models;
+
+namespace Vidlee.Dto
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+
+      //  [Min18YearsIfAMember]
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
