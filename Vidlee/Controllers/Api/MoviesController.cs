fdc07c3276b2275ff0a3e37250b8a98569eb1ff2
seanCalvey <calsean86@outlook.ie>
2020-04-06 22:24:00 +0100

    Adding API and jQuery

---
+++ Vidlee/Controllers/Api/MoviesController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidlee.Models;
+
+
+
+namespace Vidlee.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //GET Api/Movies getMovies
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            return _context.Movies.ToList();
+        }
+
+        //GET Api/Movies/{Id} getMovie
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return NotFound();
+
+            return Ok(movie);
+        }
+
+        //Post Api/Movies CreateMovie
+        public IHttpActionResult CreateMovie(Movie movie)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), movie);
+        }
+
+
+        //PUT Api/Movies/{Id} UpdateMovie
+        public IHttpActionResult UpdateMovie(int id, Movie movie)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+            movieInDb.Name = movie.Name;
+            movieInDb.ReleaseDate = movie.ReleaseDate;
+            movieInDb.GenreId = movie.GenreId;
+            movieInDb.Stock = movie.Stock;
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        //Delete Api/Movies/{Id} DeleteMovie
+
+
+    }
+}
---
+++ Vidlee/Dto/MovieDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidlee.Models;
+
+namespace Vidlee.Dto
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public int GenreId { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+      //  [Range(1, 20)]
+        public int Stock { get; set; }
+    }
+}
